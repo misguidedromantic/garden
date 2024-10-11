22b6b7ec7c11be0f89d117fe497098f9d9561b11
misguidedromantic <122736106+misguidedromantic@users.noreply.github.com>
2024-10-11 23:05:34 +1100

    New branch - mvp

---
+++ js/plans.js
@@ -0,0 +1,24 @@
+class plan {
+    
+    constructor(){
+        this.paragraphs = []
+    }
+
+
+
+}
+
+class paragraph {
+
+    constructor(text){
+        this.text = text
+    }
+
+}
+
+function addPlans (){
+
+    let planA = new plan ()
+    new plan ()
+
+}

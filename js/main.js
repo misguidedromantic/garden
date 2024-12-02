ec16f19ca4e0e90d8b4db424b14fe96b9c89ba68
misguidedromantic <122736106+misguidedromantic@users.noreply.github.com>
2024-12-02 20:58:30 +1100

    rendering subbed out

--- js/main.js
+++ js/main.js
@@ -1,35 +1,4 @@
-window.onload = function(){
-
-    function setupNavigator(){
-
-        navigation.movement = new navigatorMovement
-        navigation.menuManagement = new navigatorMenuManagement
-        navigation.contentControl = new navigatorContentControl
-
-        function createDiv(){
-            return d3.select('body')
-                .append('div')
-                .attr('id', 'navigatorDiv')
-                .style('position', 'fixed')
-                .style('border-radius', '20px')
-        }
-    
-        function createSVGCanvas(){
-            return navigatorWindow.div.append('svg')
-                .attr('id', 'navigatorSVG')
-        }
-        
-        navigatorWindow.div = createDiv()
-        navigatorWindow.svg = createSVGCanvas()
-    
-    }
-
-    setupNavigator()
-
-    navigation.loadMainMenu()
-
-
-}
+window.onload = function(){navigation.setup()}
 
 function initialSetup(){
     setupNavigator()
@@ -47,11 +16,20 @@ class navigation {
     static menuManagement = {}
     static contentControl = {}
 
+    static setup(){
+        this.rendering = new navigatorRendering
+        this.movement = new navigatorMovement
+        this.menuManagement = new navigatorMenuManagement
+        this.contentControl = new navigatorContentControl
+
+        this.rendering.createNavigator()
+        this.loadMainMenu()
+    }
+
     static loadMainMenu(){
         this.menuManagement.loadMain()
         this.contentControl.renderItems(this.menuItems)
         this.movement.toMainMenuDisplay(this.contentControl.getWidestItemWidth(), this.menuItems.length)
-        //this.contentControl.tweenTextIn()
     }
 
     
@@ -75,14 +53,14 @@ class navigation {
             this.menuManagement.loadSub(clickedItem)
             this.contentControl.renderItems(this.menuItems)
             this.movement.toSubMenuDisplay(this.contentControl.getWidestItemWidth(), this.menuItems.length)
-            //this.contentControl.tweenTextIn()
+
         }
 
         else if(itemType === 'menuItem' && subMenuLoaded){
             this.menuManagement.loadMain(clickedItem)
             this.contentControl.renderDestinations(this.menuItems)
             this.movement.toMainMenuDisplay(this.contentControl.getWidestItemWidth(), this.menuItems.length)
-            //this.contentControl.tweenTextIn()
+
         }
 
         else if(itemType === 'subMenuItem'){
@@ -96,6 +74,32 @@ class navigation {
 
 }
 
+class navigatorRendering {
+
+    updateMenu(items){
+
+    }
+
+    createNavigator(){
+        navigatorWindow.div = this.#createDiv()
+        navigatorWindow.svg = this.#createSVGCanvas()
+    }
+
+    #createDiv(){
+        return d3.select('body')
+            .append('div')
+            .attr('id', 'navigatorDiv')
+            .style('position', 'fixed')
+            .style('border-radius', '20px')
+    }
+
+    #createSVGCanvas(){
+        return navigatorWindow.div.append('svg')
+            .attr('id', 'navigatorSVG')
+    }
+
+}
+
 
 class navigatorWindow {
     static div = {}
@@ -106,6 +110,8 @@ class navigatorWindow {
     static left = 0
     static top = 0
     static padding = 20
+    static position = 'fixed'
+    static borderRadius = 20
 }
 
 
@@ -285,9 +291,23 @@ class navigatorContentControl {
 
     exitItems(selection){
 
-        function tweenTextRemovalAndColour(selection, duration) {
-            const originalText = selection.text();
-            const length = originalText.length;
+        const exitTween = this.exitTextTransitionTween
+
+        selection.each(function(){
+            const text = d3.select(this).select('text')
+            text.transition('tTweenOut')
+                .duration(100)
+                .textTween(exitTween(text, 100))
+            })
+                    
+            selection.remove()
+
+        return selection
+    }
+
+    exitTextTransitionTween(selection, duration){
+        const originalText = selection.text();
+        const length = originalText.length;
         
             selection.transition()
               .duration(duration)
@@ -300,19 +320,6 @@ class navigatorContentControl {
               .on("end", function() {
                 d3.select(this).text("");
               });
-        
-        }
-
-        selection.each(function(){
-            const text = d3.select(this).select('text')
-            text.transition('tTweenOut')
-                .duration(0)
-                .textTween(tweenTextRemovalAndColour(text, 100))
-            })
-                    
-            selection.remove()
-
-        return selection
     }
 
 
@@ -321,137 +328,6 @@ class navigatorContentControl {
         const y = i * 20 + 40
         return 'translate(' + x + ',' + y + ')'
     }
-
-
-
-
-
-
-    
-    renderDestinations(menuItems){
-
-        console.log(menuItems)
-
-        const transitions = []
-
-        function calculateTranslate(i){
-            const x = 20
-            const y = i * 20 + 40
-            return 'translate(' + x + ',' + y + ')'
-        }
-
-        function tweenTextRemovalAndColour(selection, duration) {
-            const originalText = selection.text();
-            const length = originalText.length;
-        
-            selection.transition()
-              .duration(duration)
-              .textTween(function() {
-                return function(t) {
-                  const i = Math.floor((1 - t) * length);
-                  return originalText.slice(0, i);
-                };
-              })
-              .on("end", function() {
-                d3.select(this).text("");
-              });
-        
-        }
-
-        const tColourIn = d3.transition('tColourIn')
-        const tTweenIn = d3.transition('tTweenIn')
-
-        
-        navigatorWindow.svg.selectAll('g')
-            .data(menuItems, d => d.name)
-            .join(
-                enter => {
-                    const group = enter.append('g')
-                        .attr('id', d => d.name)
-                        .attr('transform', (d, i) => calculateTranslate(i))
-                        .on('click', selectDestination)
-
-                    const text = group.append('text')
-                        .text(d => d.name)
-                        .attr('dy', '-.4em')
-                        .attr('fill', 'transparent')
-
-                    text.transition(tColourIn)
-                            .delay((d, i) => i * 100) 
-                            .duration(100)
-                            .attr('fill', 'black')
-
-                    text.transition(tTweenIn)
-                            .delay((d, i) => i * 100)
-                            .duration(200)
-                            .textTween(d => {
-                                return function(t) {
-                                    return d.name.slice(0, Math.round(t * d.name.length));
-                                };
-                            })
-                            .end()
-
-                            
-                    return group
-                },
-                
-                update => {
-                    const group = update.attr('transform', (d, i) => {return calculateTranslate(i)})
-
-                    group.select('text').attr('font-weight', d => {
-                        if(d.selected){
-                            return 'bold'
-                        } else {
-                            return 'normal'
-                        }
-                    })
-                    return group
-                }
-                    ,
-
-                exit => {
-                    const group = exit.selectAll('g')
-
-                    group.each(function(){
-                        const text = d3.select(this).select('text')
-                        text.transition('tTweenOut')
-                            .duration(0)
-                            .textTween(tweenTextRemovalAndColour(text, 100))
-                    })
-                    
-                    exit.remove()
-
-
-
-                    //transitions.push(exitTransition)
-                    
-
-                    //return exitTransition
-
-                }
-            )
-
-        //return Promise.all(transitions)
-
-    }
-
-    tweenTextIn(){
-        const selection = navigatorWindow.svg.selectAll('g').select('text').filter(d => !d.selected)
-        const t = selection
-            .attr('fill', 'black')
-            .text('')
-                .transition('tweenIn')
-                .delay(function(d, i){return i * 150})
-                .duration(200)
-                .textTween(d => {
-                    return function(t) {
-                        return d.name.slice(0, Math.round(t * d.name.length));
-                    };
-                })
-
-        return t.end()
-    }
-
 
     getWidestItemWidth(){
         const groups = navigatorWindow.svg.selectAll('g')
@@ -468,141 +344,6 @@ class navigatorContentControl {
 
         return widestWidth
     }
-}
-
-class destinationMenu {
-
-    #destinations = []
-    #plans = []
-    #items = []
-
-    constructor(){
-        this.#loadItems()
-    }
-
-    refresh(clickedItem){
-
-        if(clickedItem !== undefined){
-            this.#items = this.getItemsFromClickedMenuItem(clickedItem)
-            this.updateItemSelectionWithinType(this.getMenuItems(), clickedItem)
-        }
-
-        return this.#items
-
-    }
-
-    
-
-    updateItemSelectionWithinType(items, clickedItem){
-        items.forEach(item => {
-            if(item === clickedItem){
-                item.toggleSelection()
-            } else{
-                item.deselect()
-            }
-        })
-    }
-
-    
-
-
-
-    getItems(clickedItem){
-        let itemsArray = []
-
-        try{
-            itemsArray = this.getItemsArray(clickedItem)
-        }
-
-        catch {
-            itemsArray = this.#items
-        }
-
-        finally {
-            this.updateItems(itemsArray)
-            return itemsArray
-        }
-
-    }
-
-    updateItems(itemsArray){
-        this.#items = itemsArray
-    }
-
-    getItemsArray(clickedItem){
-        if(clickedItem.constructor.name === 'menuItem'){   
-            return this.getItemsFromClickedMenuItem(clickedItem)
-        }
-
-        if(clickedItem.constructor.name === 'subMenuItem'){
-            return this.getItemsFromClickedSubMenuItem(clickedItem)
-        }
-    }
-
-    getItemsFromClickedMenuItem(clickedMenuItem){
-
-        console.log(clickedMenuItem)
-
-        if(!clickedMenuItem.selected){  
-            return [...[clickedMenuItem], ...this.getSubMenuItems(clickedMenuItem.name)]
-        } 
-        
-        if (clickedMenuItem.selected){
-            return this.#destinations
-        }
-    }
-
-    getItemsFromClickedSubMenuItem(clickedSubMenuItem){
-        const parentItem = this.getItem(clickedSubMenuItem.parentItemName)
-        return [...[parentItem], ...this.getSubMenuItems(parentItem.name)]
-    }
-
-
-    updateItemSelection(clickedItem){
-        
-        if(clickedItem.constructor.name === 'menuItem'){
-
-        }
-        
-        
-        this.#items.forEach(item => {
-            if(item.name === itemName){
-                item.toggleSelection()
-            } else {
-                item.deselect()
-            }
-        })
-    }
-
-    getItem(itemName){
-        return this.#items.find(thisItem => thisItem.name === itemName)
-    }
-
-
-    getSubMenuItems(destinationName){
-        switch(destinationName){
-            case 'plans':
-                const planNames = plansManager.getPlanNames()
-                return planNames.map(thisPlanName => new subMenuItem(thisPlanName, destinationName))
-            default:
-                return []
-        }
-    }
-
-    #loadItems(){
-        this.#destinations = this.#getDestinations()
-        this.#plans = this.getSubMenuItems('plans')
-        this.#items = this.#destinations
-
-    }
-
-    #getDestinations(){
-        return [
-            new menuItem ('plans'),
-            new menuItem ('concepts')
-        ]
-    }
-
 }
 
 class menuItem {
@@ -727,82 +468,3 @@ class navigatorFloat {
     }
 
 }
-
-
-
-
-
-class divManager {
-    static createDiv (){
-        return d3.select('body')
-            .append('div')
-            .style('position', 'fixed')
-            .style('left', window.innerWidth / 2 + 'px')
-            .style('top', window.innerHeight / 2 + 'px')
-    }
-    
-}
-
-class canvasManager {
-    static createSVGCanvas(div){
-        return div.append('svg')    
-    }
-}
-
-
-class destinationManager{
-
-    static #destinations = ['plans','concepts']
-
-    static getDestinationNames(){
-        return this.#destinations.map(dest => new destination(dest))
-    }
-
-    static getDestinationLocations(destinationName){
-        const subLocations = this.#getSubLocations(destinationName).map(dest => new destination(dest))
-        return [...[new destination(destinationName)], ...subLocations]
-
-    }
-
-    static #getSubLocations(destinationName){
-
-        switch(destinationName){
-            
-            case 'plans':
-                return plansManager.getPlanNames()
-
-            default:
-                return []
-        }
-    }
-
-}
-
-class destination {
-    constructor(name){
-        this.name = name
-        this.selected = false
-    }
-
-}
-
-class plansManager {
-    static #plans = [
-        {displayName: 'my digital garden', fileName: 'myDigitalGarden.html'},
-        {displayName: 'wooden blocks', fileName: 'woodenBlocks.html'}
-    ]
-
-    static getPlanNames(){
-        return this.#plans.map(planData => planData.displayName)
-    }
-}
-
-/* class plan {
-    constructor(d){
-        this.id = d.id
-        this.fileName = d.fileName
-        this.displayName = d.displayName
-    }
-
-} */
-    

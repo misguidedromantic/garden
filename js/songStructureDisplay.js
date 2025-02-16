c4b1fad99384df071e78d829f994322eba7d9474
misguidedromantic <122736106+misguidedromantic@users.noreply.github.com>
2025-02-16 20:31:31 +1100

    gg

--- js/songStructureDisplay.js
+++ js/songStructureDisplay.js
@@ -2,9 +2,48 @@
 
 
 class songSectionRendering {
-    static setupStacks(song){
+    static setupStacks(song, templateSong){
+        let stackNumber = 0
+        const stacks = []
+        let currentStack = []
+
+        for(let i = 0; i < templateSong.structure.length; i++){
+            const section = templateSong.structure[i]
+
+            if(section.sequence === 1){
+                currentStack.push(section)
+                stacks[stackNumber] = currentStack
+            } else if (section.constructor.type === 'verse'){
+
+            }
+
+
+            
+           
+
+        }
+    }
+
+    static calculateStackNumberForVerse (thisVerse, allVerses){
+        const thisVerseIndex = allVerses.findIndex(section => section.verse === thisVerse)
+        let previousVerse = {}
+        try{previousVerse = allVerses[thisVerseIndex - 1]}
+        catch{}
+    }
+
+    static getPreviousSectionOfType(thisSection, allSections){
         
     }
+
+
+
+
+
+
+     
+
+    
+    
 
    
 }

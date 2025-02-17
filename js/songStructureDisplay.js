8d9adec4b84bae9119656b67189c77d1555af7c3
misguidedromantic <122736106+misguidedromantic@users.noreply.github.com>
2025-02-17 21:31:21 +1100

    stack logic

--- js/songStructureDisplay.js
+++ js/songStructureDisplay.js
@@ -2,40 +2,53 @@
 
 
 class songSectionRendering {
-    static setupStacks(song, templateSong){
+    static setupStacks(sections){
         let stackNumber = 0
-        const stacks = []
-        let currentStack = []
-
-        for(let i = 0; i < templateSong.structure.length; i++){
-            const section = templateSong.structure[i]
-
-            if(section.sequence === 1){
-                currentStack.push(section)
-                stacks[stackNumber] = currentStack
-            } else if (section.constructor.type === 'verse'){
-
-            }
-
-
+        sections.forEach(thisSection => {
+            stackNumber = stackNumber + this.getStackIncrement(thisSection, previousSection, nextSection)
+            thisSection.stackNumber = stackNumber
+        });
             
-           
+    }
 
+    static getStackIncrement(thisSection, allSections){
+        const previousSectionType = this.getSectionTypeBySequenceNumber(thisSection.sequenceNumber - 1, allSections)
+        switch(thisSection.constructor.name){
+            case 'intro':
+                return 0
+            case 'verse':
+                return this.getStackNumberForVerse(previousSectionType)
+            case 'chorus':
+                return this.getStackIncrementForChorus(previousSectionType)
+            case 'bridge':
+            case 'outro':
+                return 1
+        };
+    }
+
+    static getSectionTypeBySequenceNumber(sequenceNumber, sections){
+        if(sequenceNumber > 0 && sequenceNumber < sections.length){
+            const previousSection = sections.find(section => section.sequence = sequenceNumber)
+            return previousSection.constructor.name
         }
     }
 
-    static calculateStackNumberForVerse (thisVerse, allVerses){
-        const thisVerseIndex = allVerses.findIndex(section => section.verse === thisVerse)
-        let previousVerse = {}
-        try{previousVerse = allVerses[thisVerseIndex - 1]}
-        catch{}
+    static getStackIncrementForVerse(previousSectionType){
+        return previousSectionType === 'intro' ? 0 : 1
     }
 
-    static getPreviousSectionOfType(thisSection, allSections){
-        
+    static getStackIncrementForChorus(previousSectionType){
+        switch(previousSectionType){
+            case 'intro':
+            case 'bridge':
+            case 'chorus':
+                return 1
+            case 'verse':
+                return 0
+        }
     }
 
-
+ 
 
 
 
